deba60368edb9e048c3e8dcb60a12fcc03b2e2c3
Y Nhi Hoang <ynh0455@thi.de>
2023-11-27 10:18:26 +0100

    AB6 / Testat

--- Praktikum/Code/Worm030/board_model.c
+++ Praktikum/Code/Worm030/board_model.c
@@ -9,3 +9,28 @@
 //
 // The board model
 
+#include <curses.h>
+#include "board_model.h"
+#include "worm.h"
+
+// Place an item onto the curses display.
+void placeItem(int y, int x, chtype symbol, enum ColorPairs color_pair) {
+
+    //  Store item on the display (symbol code)
+    move(y, x);                         // Move cursor to (y,x)
+    attron(COLOR_PAIR(color_pair));     // Start writing in selected color
+    addch(symbol /*@006*/);                      // Store symbol on the virtual display
+    attroff(COLOR_PAIR(color_pair));    // Stop writing in selected color
+}
+
+// Getters
+
+// Get the last usable row on the display
+int getLastRow() {
+    return (LINES-1); //@003;
+}
+
+// Get the last usable column on the display
+int getLastCol() {
+    return (COLS-1); // @004
+}
--- Praktikum/Code/Worm030/worm_model.h
+++ Praktikum/Code/Worm030/worm_model.h
@@ -12,4 +12,29 @@
 #ifndef _WORM_MODEL_H
 #define _WORM_MODEL_H
 
+#include <stdbool.h>
+#include "worm.h"
+
+// Directions for the worm
+enum WormHeading {
+  WORM_UP,
+  WORM_DOWN,
+  WORM_LEFT,
+  WORM_RIGHT,
+  WORM_LEFT_UP,
+  WORM_RIGHT_UP,
+  WORM_LEFT_DOWN,
+  WORM_RIGHT_DOWN,
+};
+
+// Functions concerning the management of the worm data
+extern enum ResCodes initializeWorm(int len_max, int headpos_y, int headpos_x, enum WormHeading dir, enum ColorPairs color);
+
+extern void showWorm();
+extern void cleanWormTail();
+
+extern void moveWorm(enum GameStates* agame_state);
+extern bool isInUseByWorm(int new_headpos_y, int new_heapos_x);
+extern void setWormHeading(enum WormHeading dir);
+
 #endif  // #define _WORM_MODEL_H
