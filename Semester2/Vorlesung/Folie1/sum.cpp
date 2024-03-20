28afc8eab87b45262b23e016ef2dcacfa7a94853
nhixhng <hoangynhi4@gmail.com>
2024-03-20 21:31:27 +0100

    nr1

---
+++ Semester2/Vorlesung/Folie1/sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number1, number2;
+
+    cout<< "Enter two integers: ";
+    cin >> number1>> number2;
+    int sum= number1 + number2;
+
+    cout<< "sum= "<< sum<< '\n';
+
+    return 0;
+}
